2b58cc7e7ed1a930f5a9a4809ac6050d0a59d119
lherloew <lherloew@gmail.com>
2011-05-10 12:18:27 +0200

    bootstrap problem ;)

--- src/peer/PeerList.java
+++ src/peer/PeerList.java
@@ -1,6 +1,7 @@
 package peer;
 
 import java.io.*;
+import java.net.URL;
 import java.util.*;
 import java.util.Formatter;
 import java.util.logging.*;
@@ -14,6 +15,33 @@ import java.util.logging.*;
 public class PeerList {
    private Scanner input;
    private Formatter output;
+   private String filename = "peers.txt";
+
+   public PeerList() {
+      File f = new File(filename);
+      System.out.println(f + (f.exists()? " is found " : " is missing. Creating file..."));
+      try {
+
+         if(!f.exists()) {
+            f.createNewFile();
+
+            // Read peers file from jar archive
+            InputStream in = getClass().getResourceAsStream("/peer/peers");
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            
+            //output to proper file
+            while ((line = br.readLine()) != null) {
+               output = new Formatter(filename);               
+               output.format("%s\n", line);
+               output.close();
+            }                      
+         }
+      } catch (IOException io) {
+         System.out.println("File not found");
+      }
+   }
 
 
    /**
@@ -23,15 +51,12 @@ public class PeerList {
     * @param rw 0 = read, 1 = write
     */
    public void openFile(int rw) {
-      String filename = PeerList.class.getClass().getResource("/peer/peers").getPath().substring(35);
-      System.out.println(filename);
-      
       if(rw == 0) {
          try {
             input = new Scanner(new File(filename));
          } catch (FileNotFoundException ex) {
             Logger.getLogger(PeerList.class.getName()).log(Level.SEVERE, null, ex);
-         }		   
+         }
       } else {
          try {
             output = new Formatter(filename);
@@ -39,9 +64,8 @@ public class PeerList {
             Logger.getLogger(PeerList.class.getName()).log(Level.SEVERE, null, ex);
          }
       }
-
    }
-   
+
    /**
     * Creates an arraylist from text file.
     * 
@@ -51,9 +75,11 @@ public class PeerList {
    public ArrayList<Peer> readPeerList() {
       ArrayList<Peer> peerList = new ArrayList<Peer>();
       while (input.hasNext()) {
+         System.out.println("affe");
          String ip = input.next();
          int status = 0;//input.nextInt();
          peerList.add(new Peer(ip,status));
+         System.out.println(peerList.size());
       }
       return peerList;
    }
@@ -73,7 +99,7 @@ public class PeerList {
    public void closeInputFile() {
       input.close();
    }
-   
+
    public void closeOutputFile() {
       output.close();
    }
---
+++ test/config/ConfigurationTest.java
@@ -0,0 +1,69 @@
+package config;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import peer.Peer;
+
+/**
+ * <code>ConfigurationTest</code> will test the <code>Configuration</code> class
+ *
+ * @see Configuration
+ *
+ * @author Lasse
+ */
+public class ConfigurationTest {
+
+   /**
+    * Test of getConfiguration method, of class Configuration.
+    *
+    * Call getConfiguration() to load Configuration object into result and
+    * expected result.
+    *
+    * assertEquals the result from getConfiguration() with the expected result.
+    */
+   @Test
+   public void testGetConfiguration() {
+      System.out.println("Start of test: Configuration.getConfiguration()");
+      Configuration expResult = Configuration.getConfiguration();
+      Configuration result = Configuration.getConfiguration();
+      System.out.println("    Result = Expected: " + result + " = " + expResult);
+      assertEquals(expResult, result);
+      System.out.println("End test: Configuration.getConfiguration()\n");
+   }
+
+   /**
+    * Test of clone method, of class Configuration.
+    * We want this to throw an exception so we test for that.
+    * See http://junit.sourceforge.net/doc/faq/faq.htm#tests_7
+    *
+    * @throws Exception clone() has CloneNotSupportedException
+    */
+   @Test(expected=CloneNotSupportedException.class)
+   public void testClone() throws Exception {
+      System.out.println("Start and end of test: Configuration.clone()\n");
+      Configuration instance = Configuration.getConfiguration();
+      Object o = instance.clone();
+   }
+
+   /**
+    * Test of setPeers() and getPeers() method, of class Configuration.
+    */
+   @Test
+   public void testGetAndSetPeers() {
+      System.out.println("Start of test: Configuration.setPeers() and Configuration.getPeers()");
+      ArrayList<Peer> peers = new ArrayList<Peer>();
+      peers.add(new Peer("192.168.1.101", 1));
+      peers.add(new Peer("192.168.1.10", 0));      
+      Configuration instance = Configuration.getConfiguration();
+      instance.setPeers(peers);
+      ArrayList<Peer> expResult = peers;
+      ArrayList<Peer> result = instance.getPeers();
+      assertEquals(expResult, result);
+      System.out.println("    Result = Expected: " + result + " = " + expResult);
+      System.out.println("End test: Configuration.setPeers() and Configuration.getPeers()\n");
+   }
+}
